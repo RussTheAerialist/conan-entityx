e06ee702dbe1528f3e5618bec8035f5d921c3d84
Russell Hay <me@russellhay.com>
2016-10-30 13:43:24 -0700

    fixing linking

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,12 @@ class EntityX(ConanFile):
     name = 'EntityX'
     version = '1.2.0'
     settings = 'os', 'compiler', 'build_type', 'arch'
+    url = 'https://github.com/alecthomas/entityx'
+    license = 'MIT'
 
     def source(self):
     	self.run('git clone https://github.com/alecthomas/entityx.git')
+    	self.run('cd entityx && git checkout tags/{}'.format(self.version))
 
     def build(self):
     	cmake = CMake(self.settings)
@@ -19,8 +22,8 @@ class EntityX(ConanFile):
     	self.run('cmake --build . --target install {}'.format(cmake.build_config))
 
     def package(self):
-        self.copy('*.h', dst='include', src='entityx/entityx')
+        self.copy('*.h', dst='include/entityx', src='entityx/entityx')
         self.copy('*.dylib', dst='lib', src='.')  # OSX
 
     def package_info(self):
-    	pass  # Not sure what to do here
+    	self.cpp_info.libs=['entityx']
